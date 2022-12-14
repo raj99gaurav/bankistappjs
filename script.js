4e0c0888871609f28eaac500bad063ee927a72ee
raj99gaurav <rajgaurav.mail@gmail.com>
2022-12-14 17:38:02 +0530

    addding sort func

--- script.js
+++ script.js
@@ -61,11 +61,15 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  //.tectContent = 0;
+  //.textContent = 0;
 
-  movements.forEach(function (mov, i) {
+  //using slice to make copy of movements array as we dont want to change the orifinal array
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `<div class="movements__row">
@@ -220,4 +224,10 @@ btnClose.addEventListener("click", function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = "";
+});
+
+btnSort.addEventListener("click", function (e) {
+  //sorting func
+  e.preventDefault();
+  displayMovements(currentAccount.movements, true);
 });
