7c1c2fb44c953f09f4566529ee7e278fe843493d
raj99gaurav <rajgaurav.mail@gmail.com>
2022-12-14 12:30:33 +0530

    loan func

--- script.js
+++ script.js
@@ -182,6 +182,24 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+//Loan funcatinality
+
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    //Add the movement
+    currentAccount.movements.push(amount);
+
+    //Udate the UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 //close account
 
 btnClose.addEventListener("click", function (e) {
