20dc9740bd29b0d54f5cd57c76b7c4d810c74fea
raj99gaurav <rajgaurav.mail@gmail.com>
2022-12-19 09:51:06 +0530

    log in time under current balance

--- script.js
+++ script.js
@@ -161,6 +161,14 @@ currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
+const now = new Date(); //new date
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year} ,${hour}:${min}`;
+
 btnLogin.addEventListener("click", function (e) {
   e.preventDefault(); //prevent form from submitting
 
